77217474bc535b3ec0c8c67367794e85e16186bb
LukeZvada <zvadamusic@gmail.com>
2020-07-17 14:39:04 -0500

    export filter function from DataProvider

--- scripts/Business.js
+++ scripts/Business.js
@@ -7,3 +7,13 @@ export const business = (businessObj) => {
     </section> 
     `
 }
+
+export const newYorkBusiness = (nyBisinessObj) => { 
+    return `
+    <section class="nyBusiness__list"> 
+    <h2 class"business__title> ${nyBisinessObj.companyName}</h2>
+    <div class="business__street"> ${nyBisinessObj.addressFullStreet}</div>
+    <div class="business__cityState> ${nyBisinessObj.addressCity}, ${businessObj.addressStateCode}, ${businessObj.addressZipCode}
+    </section> 
+    `
+}
