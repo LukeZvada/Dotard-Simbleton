7186575169606e8b1e076efb48ad7fab12d515f6
LukeZvada <zvadamusic@gmail.com>
2020-07-22 11:01:39 -0500

    search working. changing property to be lowercase sensitive

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -36,3 +36,55 @@ export const agent = () => {
         }
     )
 }
+
+const companySearchResultArticle = document.querySelector(".foundCompanies")
+
+document
+    .querySelector("#companySearch")
+    .addEventListener("keypress", keyPressEvent => {
+        if (keyPressEvent.charCode === 13) {
+            /*
+                When user presses enter, find the matching business.
+                You can use the `.includes()` method strings to
+                see if a smaller string is part of a larger string.
+
+                Example:
+                    business.companyName.includes(keyPressEvent.target.value)
+            */
+
+            // target the #companySearch input to find what value the user typed
+            const businessArr = useBusinesses()
+            const searchValue = keyPressEvent.target.value.lowerCase();
+
+
+            const foundBusiness = businessArr.find((business) => {
+                // to prevent the capitalization issue, you could save business.companyName to a variable
+                // transform it to be all lowercase (.lowerCase())
+                const lowerCaseName = business.companyName.lowerCase()
+                // then above, make the search value all lowercase (same method)
+                // then below, 
+                return lowerCaseName.includes(searchValue)
+            });
+
+            console.log(foundBusiness)
+
+            // wrap the function below in an if statement
+            // if there is a result, show what's below
+            // if there's not, show a message that says "there are no results"
+
+            companySearchResultArticle.innerHTML = `
+                <h2>
+                ${foundBusiness.companyName}
+                </h2>
+                <section>
+                ${foundBusiness.addressFullStreet}
+
+                </section>
+                <section>
+                ${foundBusiness.addressCity},
+                ${foundBusiness.addressStateCode}
+                ${foundBusiness.addressZipCode}
+                </section>
+            `;
+        }
+    });
